49559bb817c2a629146609241fc771c12a13f56d
Justin Willis <jgw9617@gmail.com>
2016-02-20 03:14:08 -0600

    remove unused import

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,6 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import {CommandsGenerator} from "../src/commands"
-import {exec} from "child_process";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
